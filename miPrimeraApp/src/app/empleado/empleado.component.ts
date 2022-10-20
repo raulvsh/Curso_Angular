805eda21cd01bc1ae1e976c967a6334c303b583e
raulvsh <raulvsh@gmail.com>
2022-10-20 18:39:03 +0200

    Creado componente empleado

--- miPrimeraApp/src/app/empleado/empleado.component.ts
+++ miPrimeraApp/src/app/empleado/empleado.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template: "<p>Aquí iría un empleado</p>",
   styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color: red;}"]
 })
 export class EmpleadoComponent implements OnInit {
 
