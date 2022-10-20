4e40a1dc2dacb9c8ce2a70b53d8fc1b558dfb5ae
raulvsh <raulvsh@gmail.com>
2022-10-20 18:05:02 +0200

    Modificado app-title.Nueva propiedad del component

--- miPrimeraApp/src/app/app.component.ts
+++ miPrimeraApp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'miPrimeraApp';
+  saludo = 'Hola alumnos de Angular. ¿Qué tal estáis?'
 }
