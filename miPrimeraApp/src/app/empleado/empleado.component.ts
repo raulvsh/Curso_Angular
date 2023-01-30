53598780af8d73a9c7b6eda70f45c2174a802833
raulvsh <raulvsh@gmail.com>
2023-01-30 20:04:14 +0100

    Two way binding

--- miPrimeraApp/src/app/empleado/empleado.component.ts
+++ miPrimeraApp/src/app/empleado/empleado.component.ts
@@ -9,53 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
 
-  nombre="Juan";
-  apellido="Diaz";
+  nombre = "Juan";
+  apellido = "Diaz";
   //edad=28;
-  //empresa="Píldoras informáticas";
+  empresa = "asdf";
 
- /* getEdad(){
-    return this.edad;
+  /*cambiaEmpresa(event: Event) {
+    this.empresa = (<HTMLInputElement>event.target).value;
+
   }*/
+
+  /* getEdad(){
+     return this.edad;
+   }*/
 
   /*llamaEmpresa(value:String){
   }*/
 
-  habilitacionCuadro = true;
+  habilitacionCuadro = false;
 
   usuarioRegistrado = false;
 
   textoDeRegistro = "No hay nadie registrado";
 
-  getRegistroUsuario(){
-    this.usuarioRegistrado=false;
+  getRegistroUsuario() {
+    this.usuarioRegistrado = false;
   }
 
-  setUsuarioRegistrado(event:Event){
+  setUsuarioRegistrado(event: Event) {
     //this.textoDeRegistro="Hay un usuario registrado";
     //alert("Un usuario nuevo se ha registrado");
-    if((<HTMLInputElement>event.target).value=="si"){
-      this.textoDeRegistro="Hay un usuario registrado";
-    } else{
-      this.textoDeRegistro="No hay un usuario registrado";
+    if ((<HTMLInputElement>event.target).value == "si") {
+      this.textoDeRegistro = "Hay un usuario registrado";
+    } else {
+      this.textoDeRegistro = "No hay un usuario registrado";
     }
     //alert(event.returnValue)
   }
 
-  parseInt(value:string){
+  parseInt(value: string) {
     return parseInt(value);
   }
 
-  mostrarEdad(value:string){
-    let edad=parseInt(value);
+  mostrarEdad(value: string) {
+    let edad = parseInt(value);
     let mensaje;
-    if (edad<18){
-      mensaje="Menor de edad"
+    if (edad < 18) {
+      mensaje = "Menor de edad"
     }
-    else if(edad>18){
-      mensaje="Mayor de edad";
-    } else{
-      mensaje="Introduzca la edad";
+    else if (edad > 18) {
+      mensaje = "Mayor de edad";
+    } else {
+      mensaje = "Introduzca la edad";
     }
     return mensaje;
   }
