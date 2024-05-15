165b619802d8c056a87d4fce07a11a144ded438c
raulvsh <raulvsh@gmail.com>
2024-05-15 14:11:28 +0200

    Added cart button

--- store-app/src/app/app.module.ts
+++ store-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
 
@@ -20,6 +21,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
         RouterModule.forRoot([
             { path: '', component: ProductListComponent },
             { path: 'products/:productId', component: ProductDetailsComponent },
+            { path: 'cart', component: CartComponent },
 
         ]),
     ],
@@ -29,6 +31,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
         ProductListComponent,
         ProductAlertsComponent,
         ProductDetailsComponent,
+        CartComponent,
     ],
 })
 export class AppModule { }
