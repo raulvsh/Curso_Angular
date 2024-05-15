8999259c40e22bad4fc48a439e869eaf64a493f6
raulvsh <raulvsh@gmail.com>
2024-05-15 17:23:11 +0200

    Hero added

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
