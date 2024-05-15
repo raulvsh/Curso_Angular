28437574f163ba9a94cd9ca15f30ec7564b7e5c9
raulvsh <raulvsh@gmail.com>
2024-05-15 18:08:24 +0200

    2. Display a selection list

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,34 +1,28 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
-import { CommonModule, NgIf, UpperCasePipe } from '@angular/common';
+import { CommonModule, NgIf, UpperCasePipe, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {HEROES} from '../mock-heroes';
-import { NgFor } from '@angular/common';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
-    CommonModule,
     FormsModule,
     NgFor,
     NgIf,
     UpperCasePipe,
-
-
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  //hero = 'Windstorm';
-
-  /*hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }*/
-
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
