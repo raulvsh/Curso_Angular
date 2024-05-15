609f4066b49a726142684fdaefbeeae5bcff1c45
raulvsh <raulvsh@gmail.com>
2024-05-15 11:11:31 +0200

    Added  product details component

--- store-app/src/app/app.module.ts
+++ store-app/src/app/app.module.ts
@@ -7,14 +7,10 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        TopBarComponent,
-        ProductListComponent,
-        ProductAlertsComponent,
-    ],
+
     bootstrap: [
         AppComponent
     ],
@@ -23,8 +19,17 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
         ReactiveFormsModule,
         RouterModule.forRoot([
             { path: '', component: ProductListComponent },
+            { path: 'products/:productId', component: ProductDetailsComponent },
+
         ]),
-    ]
+    ],
+    declarations: [
+        AppComponent,
+        TopBarComponent,
+        ProductListComponent,
+        ProductAlertsComponent,
+        ProductDetailsComponent,
+    ],
 })
 export class AppModule { }
 
