cbf392b9687b0b6d17f86469659be95ba1a6c90c
raulvsh <raulvsh@gmail.com>
2024-05-15 17:59:33 +0200

    Heroes list and styles

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
+import { NgFor } from '@angular/common';
 
 
 @Component({
@@ -10,6 +12,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
+    NgFor,
+    NgIf,
+    UpperCasePipe,
 
 
   ],
@@ -19,9 +24,11 @@ import { FormsModule } from '@angular/forms';
 export class HeroesComponent {
   //hero = 'Windstorm';
 
-  hero: Hero = {
+  /*hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  }
+  }*/
+
+  heroes = HEROES;
 
 }
