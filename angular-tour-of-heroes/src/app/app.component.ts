014c0151f93526d4279891aba26346d5ac1cee3d
raulvsh <raulvsh@gmail.com>
2024-05-21 23:33:55 +0200

    dashboard routes added, hero routes added

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //imports: [MessagesComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
