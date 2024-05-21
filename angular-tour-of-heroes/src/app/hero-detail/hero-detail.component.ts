e79800c998c602ce9aad2a4593f0921ad03f0b88
raulvsh <raulvsh@gmail.com>
2024-05-21 21:50:37 +0200

    hero service created

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Hero} from '../hero';
+import {Component, Input} from '@angular/core';
+import {NgIf, UpperCasePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { HeroesComponent } from '../heroes/heroes.component';
+
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf, UpperCasePipe, HeroesComponent],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
+  @Input() hero?: Hero;
 
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@ import { CommonModule, NgIf, UpperCasePipe, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 
 @Component({
@@ -13,6 +14,7 @@ import { Hero } from '../hero';
     NgFor,
     NgIf,
     UpperCasePipe,
+    HeroDetailComponent
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
